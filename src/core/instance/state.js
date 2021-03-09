1f423eb1c9684bf79032244c8aab016066d7ff2e
ruijie.zhang <ruijie.zhang@newtouch.com>
2021-03-09 18:25:18 +0800

    feat: 添加注释

--- src/core/instance/state.js
+++ src/core/instance/state.js
@@ -44,7 +44,7 @@ export function proxy (target: Object, sourceKey: string, key: string) {
   }
   Object.defineProperty(target, key, sharedPropertyDefinition)
 }
-
+// 初始化状态选项
 export function initState (vm: Component) {
   vm._watchers = []
   const opts = vm.$options
