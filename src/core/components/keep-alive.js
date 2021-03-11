20993aaef583de78761e68b4e4cd7bf29526206b
jerry379 <ruijie379@163.com>
2021-03-11 22:23:33 +0800

    feat: 添加注释

--- src/core/components/keep-alive.js
+++ src/core/components/keep-alive.js
@@ -20,7 +20,10 @@ function matches (pattern: string | RegExp | Array<string>, name: string): boole
   /* istanbul ignore next */
   return false
 }
-
+/**
+ * 在该函数内对this.cache对象进行遍历，取出每一项的name值，用其与新的缓存规则进行匹配，如果匹配不上，
+ * 则表示在新的缓存规则下该组件已经不需要被缓存，则调用pruneCacheEntry函数将这个已经不需要缓存的组件实例先销毁掉，然后再将其从this.cache对象中剔除
+ */
 function pruneCache (keepAliveInstance: any, filter: Function) {
   const { cache, keys, _vnode } = keepAliveInstance
   for (const key in cache) {
@@ -41,6 +44,7 @@ function pruneCacheEntry (
   current?: VNode
 ) {
   const cached = cache[key]
+  /* 判断当前没有处于被渲染状态的组件，将其销毁*/
   if (cached && (!current || cached.tag !== current.tag)) {
     cached.componentInstance.$destroy()
   }
@@ -52,26 +56,37 @@ const patternTypes: Array<Function> = [String, RegExp, Array]
 
 export default {
   name: 'keep-alive',
-  abstract: true,
+  abstract: true,//抽象组件
 
   props: {
-    include: patternTypes,
-    exclude: patternTypes,
-    max: [String, Number]
+    include: patternTypes,//表示只有匹配到的组件会被缓存
+    exclude: patternTypes,//任何匹配到的组件都不会被缓存
+    max: [String, Number] //缓存组件的数量
   },
 
   created () {
+    /**
+     * this.cache = {
+     *    'key1':'组件1',
+     *    'key2':'组件2',
+     *    // ...
+     * }
+     */
     this.cache = Object.create(null)
+    //存储组件的key，即cache对应的键值
     this.keys = []
   },
 
   destroyed () {
+    // 将那些被缓存的并且当前没有处于被渲染状态的组件都销毁掉并将其从this.cache对象中剔除
+    // 如果include 或exclude 发生了变化，即表示定义需要缓存的组件的规则或者不需要缓存的组件的规则发生了变化，那么就执行pruneCache函数
     for (const key in this.cache) {
       pruneCacheEntry(this.cache, key, this.keys)
     }
   },
 
   mounted () {
+    // 观测 include 和 exclude 的变化
     this.$watch('include', val => {
       pruneCache(this, name => matches(val, name))
     })
@@ -81,13 +96,17 @@ export default {
   },
 
   render () {
+    /* 获取默认插槽中的第一个组件节点 */
     const slot = this.$slots.default
     const vnode: VNode = getFirstComponentChild(slot)
+    /* 获取该组件节点的componentOptions */
     const componentOptions: ?VNodeComponentOptions = vnode && vnode.componentOptions
     if (componentOptions) {
       // check pattern
+      /* 获取该组件节点的名称，优先获取组件的name字段，如果name不存在则获取组件的tag */
       const name: ?string = getComponentName(componentOptions)
       const { include, exclude } = this
+      /* 如果name不在inlcude中或者存在于exlude中则表示不缓存，直接返回vnode */
       if (
         // not included
         (include && (!name || !matches(include, name))) ||
@@ -98,25 +117,31 @@ export default {
       }
 
       const { cache, keys } = this
+      /* 获取组件的key */
       const key: ?string = vnode.key == null
         // same constructor may get registered as different local components
         // so cid alone is not enough (#3269)
         ? componentOptions.Ctor.cid + (componentOptions.tag ? `::${componentOptions.tag}` : '')
         : vnode.key
+        /* 如果命中缓存，则直接从缓存中拿 vnode 的组件实例 */
       if (cache[key]) {
         vnode.componentInstance = cache[key].componentInstance
         // make current key freshest
+        /* 调整该组件key的顺序，将其从原来的地方删掉并重新放在最后一个 */
+        //缓存淘汰策略LRU
+        // LRU（Least recently used，最近最少使用）算法根据数据的历史访问记录来进行淘汰数据，其核心思想是“如果数据最近被访问过，那么将来被访问的几率也更高”。
         remove(keys, key)
         keys.push(key)
-      } else {
+      } else {/* 如果没有命中缓存，则将其设置进缓存 */
         cache[key] = vnode
         keys.push(key)
         // prune oldest entry
+        /* 如果配置了max并且缓存的长度超过了this.max，则从缓存中删除第一个 */
         if (this.max && keys.length > parseInt(this.max)) {
           pruneCacheEntry(cache, keys[0], keys, this._vnode)
         }
       }
-
+      /* 最后设置keepAlive标记位 */
       vnode.data.keepAlive = true
     }
     return vnode || (slot && slot[0])
